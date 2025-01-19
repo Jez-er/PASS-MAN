8b34fcbe44e902e444d31bd3557cf555a7f7259b
Jez-er <andrey.chernenko008@gmail.com>
2025-01-19 13:09:19 +0200

    [FIX] Encoder warnings

--- src/utils/encoder.rs
+++ src/utils/encoder.rs
@@ -1,9 +1,9 @@
 use aes::Aes256;
+use base64::engine::general_purpose::STANDARD;
 use cipher::{BlockEncrypt, BlockDecrypt};
-use cipher::{generic_array::GenericArray, BlockSizeUser, KeyInit, BlockCipher};
-use block_padding::Pkcs7;
+use cipher::{generic_array::GenericArray, BlockSizeUser, KeyInit};
 use rand::Rng;
-use base64::{encode, decode};
+use base64::{engine::general_purpose, Engine as _};
 
 const KEY_SIZE: usize = 32; // Key length (256 bits)
 const IV_SIZE: usize = 16;  // Initialization vector length
@@ -15,12 +15,12 @@ pub fn generate_key() -> Vec<u8> {
 
 // Function for encoding in Base64
 pub fn encode_key(key: &[u8]) -> String {
-    encode(key)
+    general_purpose::STANDARD.encode(key)
 }
 
 // Function for decoding from Base64
 pub fn decode_key(encoded_key: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    Ok(decode(encoded_key)?)
+    Ok(STANDARD.decode(encoded_key)?)
 }
 
 // Generating a random initialization vector (IV)
@@ -29,11 +29,11 @@ pub fn generate_iv() -> Vec<u8> {
 }
 
 pub fn encode_iv(iv: &[u8]) -> String {
-    encode(iv)
+    general_purpose::STANDARD.encode(iv)
 }
 
 pub fn decode_iv(encoded_iv: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    Ok(decode(encoded_iv)?)
+    Ok(STANDARD.decode(encoded_iv)?)
 }
 
 /// Password encryption
@@ -43,7 +43,7 @@ pub fn encrypt_password(password: &str, key: &[u8], iv: &[u8]) -> Result<String,
     }
 
     let cipher = Aes256::new(GenericArray::from_slice(key));
-    let mut buffer = password.as_bytes().to_vec();
+    let buffer = password.as_bytes().to_vec();
     
     // Apply PKCS7 padding
     let mut padded = buffer.clone();
@@ -54,7 +54,7 @@ pub fn encrypt_password(password: &str, key: &[u8], iv: &[u8]) -> Result<String,
     // Encrypt the data
     cipher.encrypt_block(GenericArray::from_mut_slice(&mut padded));
 
-    Ok(encode(&padded)) // Returning a Base64 representation
+    Ok(general_purpose::STANDARD.encode(&padded)) // Returning a Base64 representation
 }
 
 /// Дешифрование пароля
@@ -64,7 +64,7 @@ pub fn decrypt_password(encrypted: &str, key: &[u8], iv: &[u8]) -> Result<String
     }
 
     let cipher = Aes256::new(GenericArray::from_slice(key));
-    let mut encrypted_bytes = decode(encrypted).map_err(|_| "ERROR | Failed to decode Base64")?;
+    let mut encrypted_bytes = STANDARD.decode(encrypted).map_err(|_| "ERROR | Failed to decode Base64")?;
 
     // Decrypt the data
     cipher.decrypt_block(GenericArray::from_mut_slice(&mut encrypted_bytes));
@@ -76,12 +76,4 @@ pub fn decrypt_password(encrypted: &str, key: &[u8], iv: &[u8]) -> Result<String
     // Convert the decrypted bytes into a string
     let decrypted_password = String::from_utf8(encrypted_bytes).map_err(|_| "ERROR | Failed to convert decrypted bytes to string")?;
     Ok(decrypted_password)
-}
-
-// Password comparison
-pub fn compare_passwords(input_password: &str, encrypted_password: &str, key: &[u8], iv: &[u8]) -> bool {
-    match decrypt_password(encrypted_password, key, iv) {
-        Ok(decrypted_password) => decrypted_password == input_password,
-        Err(_) => false,
-    }
 }
